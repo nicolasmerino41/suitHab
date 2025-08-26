7b75b618f592b5cc2b39a80be9b1a17e2d7f8544
nicomerino <nicolasmerino41@gmail.com>
2025-08-26 15:44:12 +0200

    Create SetUp.jl

---
+++ SetUp.jl
@@ -0,0 +1,9 @@
+@time begin
+    PC = "nicol"
+    using Random, CairoMakie, StatsBase, Statistics
+    using Pkg
+    Pkg.activate(joinpath("C:\\Users", PC, "OneDrive\\PhD\\GitHub\\simBio2"))
+    cd(joinpath("C:\\Users", PC, "OneDrive\\PhD\\GitHub\\simBio2"))
+    meta_path = joinpath("C:\\Users", PC, "OneDrive\\PhD\\Metaweb Modelling")
+    import Base.Threads: @threads
+end
