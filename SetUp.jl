36e933d18c59fd8db5c4e0e91fbe0b84c33c82cd
Nico Merino <nicolasmerino41@gmail.com>
2025-09-03 16:16:04 +0200

    Update SetUp.jl

--- SetUp.jl
+++ SetUp.jl
@@ -2,6 +2,7 @@
     PC = ENV["USERNAME"]
     using Pkg
     using Random, CairoMakie, StatsBase, Statistics
+    using DataFrames, CSV
     Pkg.activate(joinpath("C:\\Users", PC, "OneDrive\\PhD\\GitHub\\suitHab"))
     cd(joinpath("C:\\Users", PC, "OneDrive\\PhD\\GitHub\\suitHab"))
     import Base.Threads: @threads
