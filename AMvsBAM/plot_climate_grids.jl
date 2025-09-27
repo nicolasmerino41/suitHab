36fe82792b36f4a3f8474ea8e2f6b79ff312cdaf
nicomerino <nicolasmerino41@gmail.com>
2025-09-27 11:54:26 +0200

    Create plot_climate_grids.jl

---
+++ AMvsBAM/plot_climate_grids.jl
@@ -0,0 +1,37 @@
+function plot_climate_grids(grid_grad, grid_ridge, grid_fractal; outpath=nothing, clim=nothing, titles=nothing)
+    # Adapter: accept matrices or structs with values/nx/ny
+    to_matrix(g) = g isa AbstractMatrix ? g :
+                   reshape(getfield(g, :values), getfield(g, :nx), getfield(g, :ny))
+
+    Gs = (to_matrix(grid_grad), to_matrix(grid_ridge), to_matrix(grid_fractal))
+
+    # Determine color limits if not provided
+    if clim === nothing
+        gmin = minimum(minimum, Gs)
+        gmax = maximum(maximum, Gs)
+        clim = (float(gmin), float(gmax))
+    end
+
+    # Titles
+    titles === nothing && (titles = ["Gradient","Ridge","Fractal"])
+
+    fig = Figure(; size=(1200, 420))
+
+    for (j, (g, title)) in enumerate(zip(Gs, titles))
+        ax = Axis(fig[1, j], title=title, xlabel="x", ylabel="y")
+        heatmap!(ax, g; colormap=:viridis, colorrange=clim)
+    end
+
+    Colorbar(fig[:, end + 1], label="Climate value", colorrange=clim)
+    fig[0, :] = Label(fig, "Areas of study (climate)", fontsize=22, tellwidth=false)
+
+    if outpath !== nothing
+        save(outpath, fig)
+    end
+    display(fig)
+    return fig
+end
+
+# Example (note the variable name fix):
+plot_climate_grids(grid_gradient, grid_ridge, Cgrid; outpath="AMvsBAM/data/figs/final/climate_grids.png")
+
