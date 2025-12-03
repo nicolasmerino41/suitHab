8a12722de8c1ca79a625a6256e2db32e257411d6
nicomerino <nicolasmerino41@gmail.com>
2025-12-03 17:54:45 +0100

    Update Plotting.jl

--- suitHab2/src/Visualisation/Plotting.jl
+++ suitHab2/src/Visualisation/Plotting.jl
@@ -5,7 +5,7 @@ export plot_grid
 using CairoMakie
 
 function plot_grid(G; title="grid")
-    fig = Figure()
+    fig = Figure(; size = (1100, 600))
     ax = Axis(fig[1,1]; title=title)
     heatmap!(ax, G; colormap=:viridis, colorrange=(0, 1))
     display(fig)
