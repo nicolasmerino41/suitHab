ae1d738e124b2db7da5da44d6b40cec128111074
nicomerino <nicolasmerino41@gmail.com>
2026-02-10 14:57:06 +0100

    Update MatchBetweenThermoFresh&OldenLinks.jl

--- FebruaryRestart/MatchBetweenThermoFresh&OldenLinks.jl
+++ FebruaryRestart/MatchBetweenThermoFresh&OldenLinks.jl
@@ -21,3 +21,22 @@ overlap = innerjoin(mw_tf2, mw_old2, on=[:pred, :prey])
 
 first(overlap, 50) |> display
 CSV.write("overlap_tf_olden_edges.csv", overlap)
+
+# ---- Unique species (nodes) in each metaweb ----
+tf_species = Set(vcat(mw_tf2.pred, mw_tf2.prey))
+old_species = Set(vcat(mw_old2.pred, mw_old2.prey))
+
+# Species in ThermoFresh×GloBI but NOT in Olden×GloBI
+tf_not_in_old = setdiff(tf_species, old_species)
+
+# Species in Olden×GloBI but NOT in ThermoFresh×GloBI
+old_not_in_tf = setdiff(old_species, tf_species)
+
+@info "TF unique species (pred ∪ prey)" length(tf_species)
+@info "Olden unique species (pred ∪ prey)" length(old_species)
+@info "Species in TF but not in Olden" length(tf_not_in_old)
+@info "Species in Olden but not in TF" length(old_not_in_tf)
+
+# Optional: inspect / save list
+first(sort!(collect(tf_not_in_old)), 50) |> display
+CSV.write("tf_species_not_in_olden.csv", DataFrame(species = sort!(collect(tf_not_in_old))))
